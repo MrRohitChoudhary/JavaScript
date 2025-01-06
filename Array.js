a6cdd12b478dc2ed9976f74d028fd1e88d54bbb5
MrRohitChoudhary <156509619+MrRohitChoudhary@users.noreply.github.com>
2025-01-06 09:22:28 +0000

    I Learned String &  Array

---
+++ Array.js
@@ -0,0 +1,3 @@
+let Fruits = ["Mango","Apple","Grapes","Banana","Dragon Fruit"];
+Fruits.pop();
+console.log(Fruits);
---
+++ String.js
@@ -0,0 +1,8 @@
+let str1= "I'm Professional    ";
+let str = "JavaScript Programmer";
+// console.log(str1.concat(str));  this is used to for concatination of two words or sentences (joining )
+// console.log(str.indexOf("r"));  This method is used to find the index of a specific character or substring in a string. For example, the following code will return 6:
+// more methods are 1.slice 2.replace 3.toUpperCase 4.toLowercase
+
+
+
