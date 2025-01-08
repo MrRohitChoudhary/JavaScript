55519704d163ae1d63a23eb5f20936da646cceca
MrRohitChoudhary <156509619+MrRohitChoudhary@users.noreply.github.com>
2025-01-08 16:44:40 +0000

    Added Number Method in the file

---
+++ number.js
@@ -0,0 +1,8 @@
+//By Using Number() function we can convert the string to number
+
+console.log(Number("3.14"));
+
+//You can also use the parseInt() and parseFloat() methods to convert strings to integers and floating-point numbers, respectively.
+
+console.log(parseInt("9248"));
+console.log(parseFloat("3.14"));
