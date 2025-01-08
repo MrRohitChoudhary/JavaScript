b823a43ed6a5c08a1d11dbb08c0188413161df55
MrRohitChoudhary <156509619+MrRohitChoudhary@users.noreply.github.com>
2025-01-08 11:06:34 +0000

    linked Html to the Javascript

--- Array.js
+++ Array.js
@@ -1,3 +1,21 @@
-let Fruits = ["Mango","Apple","Grapes","Banana","Dragon Fruit"];
-Fruits.pop();
-console.log(Fruits);
+// let Fruits = ["Mango","Apple","Grapes","Banana","Dragon Fruit"];
+// // Fruits.pop(); pop method is used delete the last item in the Arrray
+// console.log(Fruits);
+let Student = [[1,2,3,], "Rohit","Pravin","Subham"]
+// Student.pop();
+//  Student.shift("Dalle");
+//Student.unshift("Dalle");// this will add extra  space in the beginning 
+// slice is use to access the particular item from the array
+// console.log(Student.splice(1,4));
+
+//Array With Loops
+
+var meraBetaPravin = [1,2,3,4,5];
+for (var i=0; i<meraBetaPravin.length;i++){
+    console.log(meraBetaPravin[i]);
+}
+
+document.querySelector('.pravin').addEventListener('click',function(){
+    this.style.backgroundColor = "black";
+    this.style.color = "yellow";
+})
