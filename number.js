4119a6000fd0f825edf42a95a662b2159a988c48
MrRohitChoudhary <156509619+MrRohitChoudhary@users.noreply.github.com>
2025-01-08 16:53:51 +0000

    All Number Method Is Added In the File

--- number.js
+++ number.js
@@ -6,3 +6,12 @@ console.log(Number("3.14"));
 
 console.log(parseInt("9248"));
 console.log(parseFloat("3.14"));
+
+// let num = 7;
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+//tofixed method
+
+let Number1 = 2.564644646;
+console.log(Number1.toFixed(5));
