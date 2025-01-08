e2c672786a718327f67def058cd655cac12eb5b4
MrRohitChoudhary <156509619+MrRohitChoudhary@users.noreply.github.com>
2025-01-08 16:34:18 +0000

    we Have learned Math Method of the Javascipt

--- Math.js
+++ Math.js
@@ -1,1 +1,21 @@
-safasf
+//To get a random number
+
+console.log(Math.random()*10);// here i have multiplied it by 10 to get a random number between 0 to 10
+console.log(Math.random()*100);// here i have multiplied it by 100 to get a random number between 0 to 100
+
+
+//floor method and Ceil method
+console.log("floor method and Ceil method");
+
+console.log(Math.floor(2.9));
+console.log(Math.ceil(100.2));
+
+//Max and Min method
+
+console.log("Max and Min method");
+console.log(Math.max(1,2,3,4,5,6,7,8,9,10));
+console.log(Math.min(1,2,3,4,5,6,7,8,9,10));
+
+//Square root method
+console.log("Square root method");
+console.log(Math.sqrt(9));
